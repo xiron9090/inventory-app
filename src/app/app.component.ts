0a1f7f587e7722929257978d836d0f6edd7e7b18
xiron9090 <xiron9090@gamil.com>
2020-04-08 05:08:12 -0400

    fixed data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,19 +20,19 @@ export class AppComponent {
       new Product(
         'MYSHOES',
         'Black Running Shoes',
-        '/assets/images/products/black-shoes.jpg',
+        './assets/images/products/black-shoes.jpg',
         ['Men', 'Shoes', 'Running Shoes'],
         109.99),
       new Product(
         'NEATOJACKET',
         'Blue Jacket',
-        '/assets/images/products/blue-jacket.jpg',
+        './assets/images/products/blue-jacket.jpg',
         ['Women', 'Apparel', 'Jackets & Vests'],
         238.99),
       new Product(
         'NICEHAT',
         'A Nice Black Hat',
-        '/assets/images/products/black-hat.jpg',
+        './assets/images/products/black-hat.jpg',
         ['Men', 'Accessories', 'Hats'],
         29.99)
       ];

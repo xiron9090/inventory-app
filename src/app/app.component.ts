7ca3d12424b9599d52dda36e9e82b522c04b6abc
xiron9090 <xiron9090@gamil.com>
2020-04-08 04:56:41 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  EventEmitter
+} from '@angular/core';
 
+import { Product } from './product.model';
+
+/**
+ * @InventoryApp: the top-level component for our application
+ */
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'inventory-app-root',
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'inventory-app';
+  products: Product[];
+
+  constructor() {
+    this.products = [
+      new Product(
+        'MYSHOES',
+        'Black Running Shoes',
+        '/assets/images/products/black-shoes.jpg',
+        ['Men', 'Shoes', 'Running Shoes'],
+        109.99),
+      new Product(
+        'NEATOJACKET',
+        'Blue Jacket',
+        '/assets/images/products/blue-jacket.jpg',
+        ['Women', 'Apparel', 'Jackets & Vests'],
+        238.99),
+      new Product(
+        'NICEHAT',
+        'A Nice Black Hat',
+        '/assets/images/products/black-hat.jpg',
+        ['Men', 'Accessories', 'Hats'],
+        29.99)
+      ];
+  }
+
+  productWasSelected(product: Product): void {
+    console.log('Product clicked: ', product);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsListComponent } from './product-list/products-list.component'
+import { ProductRowComponent } from './product-row/product-row.component';
+import { ProductDepartmentComponent } from './product-deparment/product-department.component';
+import { PriceDisplayComponent } from './price-display/price-display.component';
+import { ProductImageComponent } from './product-image/product-image.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductImageComponent,
+    ProductDepartmentComponent,
+    PriceDisplayComponent,
+    ProductRowComponent,
+    ProductsListComponent
   ],
   imports: [
     BrowserModule,
